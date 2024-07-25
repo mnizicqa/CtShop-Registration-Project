3ff5e954c24ef8b876a94d4e1921bd38ed92c42f
Mario Nizic <marionizic@msn.com>
2024-07-25 16:05:50 +0200

    Reapply "Added method for reading JSON file for succesful registration, added data driven test for succesfull registration"
    This reverts commit 486b5191e9f209f9b35b9df6164b9080b26ab254.

---
+++ src/test/java/utils/DataUtilSuccessfulRegistration.java
@@ -0,0 +1,47 @@
+package utils;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.DataProvider;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+public class DataUtilSuccessfulRegistration {
+
+    @DataProvider
+    public Object[] dataProviderSuccessfulRegistration() {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = null;
+
+        // Read JSON file
+        try {
+            Object obj = parser.parse(new FileReader("src/test/test_data/testDataSuccessfulRegistration.json"));
+            jsonObject = (JSONObject) obj;
+        } catch (IOException | ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        // Array to store JSON data
+        Object[] data = new Object[1];
+
+        // Store JSON data as key/value pair in HashMap
+        HashMap<String, String> hashMap = new LinkedHashMap<>();
+        if (jsonObject != null) {
+            Set<String> jsonObjKeys = jsonObject.keySet();
+            for (String jsonObjKey : jsonObjKeys) {
+                hashMap.put(jsonObjKey, (String) jsonObject.get(jsonObjKey));
+            }
+        } else {
+            throw new RuntimeException();
+        }
+
+        //Store HashMap in an array and return it
+        data[0] = hashMap;
+        return data;
+    }
+}
